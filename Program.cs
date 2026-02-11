a4bdbf1b4e0264b1a116ffa22f10b83df8e11dc0
Carlos <carlosaugustoslima@gmail.com>
2026-02-11 20:03:11 -0300

    Resolução da primeira questão

--- Program.cs
+++ Program.cs
@@ -3,4 +3,28 @@
 //Lista de Exercício lógica c#
 //Nível 1: Variáveis e Operações Básicas
 
-#region
+#region 1. Calculadora de Média Simples 
+
+Console.Write("Digite sua primeira nota: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite sua segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite sua terceira nota: ");
+double nota3 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite sua quarta nota: ");
+double nota4 = Convert.ToDouble(Console.ReadLine());
+
+double media = (nota1 + nota2 + nota3 + nota4)/4;
+
+Console.Write("A sua Média é:");
+Console.Write(media);
+Console.ReadKey();
+
+
+#endregion
